226fd47f755cb707a7873c2128f8754e861fb4d6
kein_name <andreea.a.musat@gmail.com>
2020-05-25 20:31:31 +0300

    cleanup+req.txt

--- src/collect_maze_data.py
+++ src/collect_maze_data.py
@@ -1,3 +1,26 @@
+"""
+Data collection script. The data will be stored in the following way:
+
+- root_dir
+    - env_0
+        - ep_0
+            - 0_*.png
+            - 1_*.png
+            - ...
+        - ep_1
+            - 0_*.png
+            - 1_*.png
+            ...
+        - ...
+    - env_1
+        - ep_1
+            - 0_*.png
+            - 1_*.png
+            - ...
+        - ...
+    - ...
+"""
+
 import matplotlib
 import os
 import shutil
@@ -8,53 +31,61 @@ import gym_maze
 from matplotlib import image
 from skimage import color
 
+
 num_envs = 1000
 num_episodes = 50
 max_frames = 2500
 
-data_path = os.path.join('.', 'maze_data')
+# Warning: this code will delete all existent data in
+# data_path and will start collecting the data again.
+# Put a dummy folder name to avoid deleting everything.
+data_path = os.path.join('.', 'TODO-CHANGE-THIS-NAME')
 if not os.path.exists(data_path):
-	os.mkdir(data_path)
+    os.mkdir(data_path)
 
 for env_no in range(num_envs):
-	env = gym.make("maze-random-10x10-plus-v0")
-	env_data_dir = os.path.join(data_path, 'env_' + str(env_no))
-	if os.path.exists(env_data_dir):
-		shutil.rmtree(env_data_dir)
-	os.mkdir(env_data_dir)		
+    env = gym.make("maze-random-10x10-plus-v0")
+    env_data_dir = os.path.join(data_path, 'env_' + str(env_no))
+    if os.path.exists(env_data_dir):
+        shutil.rmtree(env_data_dir)
+    os.mkdir(env_data_dir)        
 
-	for ep_no in range(num_episodes):
-		ep_dir = os.path.join(env_data_dir, 'ep_' + str(ep_no))
-		if os.path.exists(ep_dir):
-			shutil.rmtree(ep_dir)
-		os.mkdir(ep_dir)				
+    for ep_no in range(num_episodes):
+        ep_dir = os.path.join(env_data_dir, 'ep_' + str(ep_no))
+        if os.path.exists(ep_dir):
+            shutil.rmtree(ep_dir)
+        os.mkdir(ep_dir)                
 
-		obs = env.reset()
-		for frame_no in range(max_frames):
-			pixels = env.render(mode='rgb_array')
-			pixels = color.rgb2gray(pixels)
+        obs = env.reset()
+        for frame_no in range(max_frames):
+            pixels = env.render(mode='rgb_array')
+            pixels = color.rgb2gray(pixels)
 
-			action = env.action_space.sample()
-			obs, reward, done, _ = env.step(action)
+            action = env.action_space.sample()
+            obs, reward, done, _ = env.step(action)
 
-			f_name = str(frame_no) + '_' + str(action)
-			f_path = os.path.join(ep_dir, f_name + '.png')
-			matplotlib.image.imsave(f_path, pixels)
+            # Save the name of the current frame in the format
+            # XXX_Y.png, where XXX is the current frame number and
+            # Y is the current action the agent took.
+            f_name = str(frame_no) + '_' + str(action)
+            f_path = os.path.join(ep_dir, f_name + '.png')
+            matplotlib.image.imsave(f_path, pixels)
 
-			if done:
-				frame_no += 1
-				pixels = env.render(mode='rgb_array')
-				pixels = color.rgb2gray(pixels)
-				action = env.action_space.sample()
-				f_name = str(frame_no) + '_' + str(action)
-				f_path = os.path.join(ep_dir, f_name + '.png')
-				matplotlib.image.imsave(f_path, pixels)
+            # Also save the last frame of the game.
+            if done:
+                frame_no += 1
+                pixels = env.render(mode='rgb_array')
+                pixels = color.rgb2gray(pixels)
+                action = env.action_space.sample()
+                f_name = str(frame_no) + '_' + str(action)
+                f_path = os.path.join(ep_dir, f_name + '.png')
+                matplotlib.image.imsave(f_path, pixels)
 
-				print('Finished episode %d after %d steps' % (
-					ep_no,
-					frame_no
-				))
-				
-				break
+                print('Finished episode %d after %d steps' % (
+                    ep_no,
+                    frame_no
+                ))
+                
+                break
 
-	del env
+    del env
--- src/maze_dataset.py
+++ src/maze_dataset.py
@@ -20,149 +20,149 @@ def natural_keys(text):
 
 class MazeDataset(torch.utils.data.Dataset):
 
-	def __init__(self, root_dir, transform=None):
-		self.transform = transform
-		self.frames_names = MazeDataset.read_dir(root_dir)
+    def __init__(self, root_dir, transform=None):
+        self.transform = transform
+        self.frames_names = MazeDataset.read_dir(root_dir)
 
-	@staticmethod
-	def read_dir(root_dir):
-		"""Retrieve all names of collected frames.
+    @staticmethod
+    def read_dir(root_dir):
+        """Retrieve all names of collected frames.
 
-		We assume the following hierarchy for the collected data:
+        We assume the following hierarchy for the collected data:
 
-		- root_dir
-			- env_0
-				- ep_0
-					- 0_*.png
-					- 1_*.png
-					- ...
-				- ep_1
-					- 0_*.png
-					- 1_*.png
-					...
-				- ...
-			- env_1
-				- ep_1
-					- 0_*.png
-					- 1_*.png
-					- ...
-				- ...
-			- ...
+        - root_dir
+            - env_0
+                - ep_0
+                    - 0_*.png
+                    - 1_*.png
+                    - ...
+                - ep_1
+                    - 0_*.png
+                    - 1_*.png
+                    ...
+                - ...
+            - env_1
+                - ep_1
+                    - 0_*.png
+                    - 1_*.png
+                    - ...
+                - ...
+            - ...
 
-		@param root_dir: str, the root of the file path where the
-			data was collected.
+        @param root_dir: str, the root of the file path where the
+            data was collected.
 
-		@returns frames_paths: List[str], the full paths of all frames which
-			would represent a valid last frame in a stack of 4.
-		"""
-		frames_paths = []
-		envs = sorted(os.listdir(root_dir), key=natural_keys)
-		for env_id in envs:
-			env_path = os.path.join(root_dir, str(env_id))
-			eps = sorted(os.listdir(env_path), key=natural_keys)
-			for ep_id in eps:
-				ep_path = os.path.join(env_path, str(ep_id))
-				crt_frames = os.listdir(ep_path)
-				crt_frames = sorted(crt_frames, key=natural_keys)
+        @returns frames_paths: List[str], the full paths of all frames which
+            would represent a valid last frame in a stack of 4.
+        """
+        frames_paths = []
+        envs = sorted(os.listdir(root_dir), key=natural_keys)
+        for env_id in envs:
+            env_path = os.path.join(root_dir, str(env_id))
+            eps = sorted(os.listdir(env_path), key=natural_keys)
+            for ep_id in eps:
+                ep_path = os.path.join(env_path, str(ep_id))
+                crt_frames = os.listdir(ep_path)
+                crt_frames = sorted(crt_frames, key=natural_keys)
 
-				for frame in crt_frames[3:-1]:
-					frame_path = os.path.join(ep_path, frame)
-					frames_paths.append(frame_path)
+                for frame in crt_frames[3:-1]:
+                    frame_path = os.path.join(ep_path, frame)
+                    frames_paths.append(frame_path)
 
-		return frames_paths
+        return frames_paths
 
-	def get_frame_path(self, frame_dir, frame_num):
-		"""Get the full frame path, given a directory and a frame name.
+    def get_frame_path(self, frame_dir, frame_num):
+        """Get the full frame path, given a directory and a frame name.
 
-		Because the frame names are in the format 'XXX_Y', where XXX is
-		the frame index (might have more or less digits) and Y is the 
-		action number, we need to be able to retrieve a frame only given
-		the frame number, but not the action.
+        Because the frame names are in the format 'XXX_Y', where XXX is
+        the frame index (might have more or less digits) and Y is the 
+        action number, we need to be able to retrieve a frame only given
+        the frame number, but not the action.
 
-		@param frame_dir: str, representing the directory where the frame
-			should be found.
-		@param frame_num: str, representing the frame index we're 
-			looking for.
+        @param frame_dir: str, representing the directory where the frame
+            should be found.
+        @param frame_num: str, representing the frame index we're 
+            looking for.
 
-		@returns frame_path: str, a file path inside frame_dir which
-			matches the regular expression 'XXX_*', where XXX is the
-			frame number. 
-		"""
-		frame_name_re = str(frame_num) + '_*'
-		frame_path_re = os.path.join(frame_dir, frame_name_re)
-		frame_path = glob.glob(frame_path_re)[0]
+        @returns frame_path: str, a file path inside frame_dir which
+            matches the regular expression 'XXX_*', where XXX is the
+            frame number. 
+        """
+        frame_name_re = str(frame_num) + '_*'
+        frame_path_re = os.path.join(frame_dir, frame_name_re)
+        frame_path = glob.glob(frame_path_re)[0]
 
-		return frame_path
+        return frame_path
 
-	def load_transformed_frame(self, frame_path):
-		"""Load a frame and transform it if neccessary.
+    def load_transformed_frame(self, frame_path):
+        """Load a frame and transform it if neccessary.
 
-		@param frame_path: str, the name of the frame to be read.
-		@returns img: np.array with values between 0 and 1 representing
-			the black and white pixel values of the frame.
-		"""
-		img = Image.open(frame_path)
-		img = np.array(img).astype(float) / 255.0
+        @param frame_path: str, the name of the frame to be read.
+        @returns img: np.array with values between 0 and 1 representing
+            the black and white pixel values of the frame.
+        """
+        img = Image.open(frame_path)
+        img = np.array(img).astype(float) / 255.0
 
-		if len(img.shape) == 3:
-			img = color.rgb2gray(img)
-		
-		if self.transform is not None:
-			img = self.transform(img)
+        if len(img.shape) == 3:
+            img = color.rgb2gray(img)
+        
+        if self.transform is not None:
+            img = self.transform(img)
 
-		return img
+        return img
 
-	def __getitem__(self, index):
-		"""Returns a dictionary containing a dataset example.
+    def __getitem__(self, index):
+        """Returns a dictionary containing a dataset example.
 
-		@param index: int, the index of the dataset example to be returned.
-			The index will be used for retrieving the name of the last 
-			image in the stack of four images.
-		@returns data_dict: dictionary containing the following elements:
-			'curr_state': np.array of shape [4, 640, 640] (if a resize 
-				transform was not applied, otherwise it might vary).
-			'action': int, the action that the agent took on the last frame
-				from the current stack of frames.
-			'next_frame': np.array of shape [640, 640] (again, if a resize
-				transform was not applied)
-		"""
-		data_dict = {
-			'curr_state' : [],
-			'action' : 0,
-			'next_frame' : None
-		}
+        @param index: int, the index of the dataset example to be returned.
+            The index will be used for retrieving the name of the last 
+            image in the stack of four images.
+        @returns data_dict: dictionary containing the following elements:
+            'curr_state': np.array of shape [4, 640, 640] (if a resize 
+                transform was not applied, otherwise it might vary).
+            'action': int, the action that the agent took on the last frame
+                from the current stack of frames.
+            'next_frame': np.array of shape [640, 640] (again, if a resize
+                transform was not applied)
+        """
+        data_dict = {
+            'curr_state' : [],
+            'action' : 0,
+            'next_frame' : None
+        }
 
-		# Get the name of the last frame in the stack and the full
-		# path of the directory where it's found.
-		last_frame_path = self.frames_names[index]
-		last_dir_separator = last_frame_path.rfind('/')
-		last_frame_dir = last_frame_path[:last_dir_separator]
-		
-		# Extract all numbers from the full frame path.
-		nums = re.findall(r'-?\d+\.?\d*', last_frame_path)
-		_, _, last_frame_num, action_num = nums
-		last_frame_num = int(last_frame_num)
-		action_num = int(action_num[:-1])
+        # Get the name of the last frame in the stack and the full
+        # path of the directory where it's found.
+        last_frame_path = self.frames_names[index]
+        last_dir_separator = last_frame_path.rfind('/')
+        last_frame_dir = last_frame_path[:last_dir_separator]
+        
+        # Extract all numbers from the full frame path.
+        nums = re.findall(r'-?\d+\.?\d*', last_frame_path)
+        _, _, last_frame_num, action_num = nums
+        last_frame_num = int(last_frame_num)
+        action_num = int(action_num[:-1])
 
-		# Get the names of all names in the stack (so 3 frames in the past
-		# plus the last_frame_path) and load them.
-		for frame_num in range(last_frame_num - 3, last_frame_num + 1):
-			frame_path = self.get_frame_path(last_frame_dir, frame_num)
-			frame = self.load_transformed_frame(frame_path)
-			data_dict['curr_state'].append(frame)
+        # Get the names of all names in the stack (so 3 frames in the past
+        # plus the last_frame_path) and load them.
+        for frame_num in range(last_frame_num - 3, last_frame_num + 1):
+            frame_path = self.get_frame_path(last_frame_dir, frame_num)
+            frame = self.load_transformed_frame(frame_path)
+            data_dict['curr_state'].append(frame)
 
-		# Load the next frame.
-		next_frame_path = self.get_frame_path(last_frame_dir, last_frame_num + 1)
-		next_frame = self.load_transformed_frame(next_frame_path)
-		
-		data_dict['next_frame'] = next_frame
-		data_dict['action'] = action_num
-		data_dict['curr_state'] = np.array(data_dict['curr_state'])
+        # Load the next frame.
+        next_frame_path = self.get_frame_path(last_frame_dir, last_frame_num + 1)
+        next_frame = self.load_transformed_frame(next_frame_path)
+        
+        data_dict['next_frame'] = next_frame
+        data_dict['action'] = action_num
+        data_dict['curr_state'] = np.array(data_dict['curr_state'])
 
-		return data_dict
+        return data_dict
 
-	def __len__(self):
-		return len(self.frames_names)
+    def __len__(self):
+        return len(self.frames_names)
 
 
 
